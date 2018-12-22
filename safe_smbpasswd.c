ab2eb14556d781021f1630cba433e4f99c58a1a9
lbrayner <lbrayner@users.noreply.github.com>
2018-12-22 19:43:30 -0300

    Prints the command
    Tests the second optional argument.

--- safe_smbpasswd.c
+++ safe_smbpasswd.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+/* #include <regex.h> */ 
 #include <string.h>
+
+/* Runs /usr/bin/smbpasswd -s (-x|-a)? USERNAME
+ * */
+
+char * usage()
+{
+    return "./safe_smbpasswd USERNAME\n";
+}
+
+int streq(char * left, char * right)
+{
+    return strcmp(left,right) == 0;
+}
 
 int main (int argc, char *argv[])
 {
+    const char * const prefix = "/usr/bin/smbpasswd -s";
+    char optional[4] = "";
+    char command[300] = "";
 
-     if (argc != 2) {
-       printf ("USAGE: ./Person [-f|-l]\n");
-       return 1;
-     }
-     if (strcmp (argv[1], "-f") == 0) {
-       printf("First Name");
-     }
-     else if (strcmp (argv[1], "-l") == 0)
-       printf("Last Name");
-     }
-     else {
-       printf ("Unknown argument\n");
-       return 1;
-     }
-     return 0;
+    if(argc < 2 || argc > 3)
+    {
+        printf(usage());
+        return 1;
+    }
+
+    if(argc == 3 && !streq(argv[1], "-a") && !streq(argv[1],"-x"))
+    {
+        printf(usage());
+        return 1;
+    }
+
+    if(argc == 3)
+        sprintf(optional," %s",argv[1]);
+
+    sprintf(command,"%s%s\n",prefix,optional);
+
+    printf(command);
+
+    return 0;
 }
